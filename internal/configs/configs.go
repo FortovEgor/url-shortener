275094a4b64704361ecfb76c6f3af55c14a9af93
Egor Fortov <egorfortov@gmail.com>
2023-04-06 15:10:37 +0300

    deleted unneeded comments

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -1,12 +1,6 @@
 package configs
 
-//const Port = ":8080"
-//const Host = "http://localhost" + Port + "/"
-
 type Config struct {
-	//Port          string `env:"PORT" envDefault:":8080"`
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-
-	//Host string `env:"HOST" envDefault:"http://localhost:8080/"`
 }
