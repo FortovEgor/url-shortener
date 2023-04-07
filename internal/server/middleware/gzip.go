4949102f68d28293e68b210855cfa12d8f5be2ed
Egor Fortov <egorfortov@gmail.com>
2023-04-08 00:16:18 +0300

    very bad things

--- internal/server/middleware/gzip.go
+++ internal/server/middleware/gzip.go
@@ -1,8 +1,10 @@
 package gzip
 
 import (
+	"bytes"
 	"compress/gzip"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -18,24 +20,44 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func GZIPHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// проверяем, что клиент поддерживает gzip-сжатие
+
+		// gzip Decode
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer func() {
+				_ = gz.Close()
+			}()
+
+			body, err := io.ReadAll(gz)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			r.Body = ioutil.NopCloser(bytes.NewReader(body))
+			r.ContentLength = int64(len(body))
+		}
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			// если gzip не поддерживается, передаём управление
-			// дальше без изменений
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		// создаём gzip.Writer поверх текущего w
 		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
 		if err != nil {
-			io.WriteString(w, err.Error())
+			if _, err := io.WriteString(w, err.Error()); err != nil {
+				return
+			}
 			return
 		}
-		defer gz.Close()
+		defer func() {
+			_ = gz.Close()
+		}()
 
 		w.Header().Set("Content-Encoding", "gzip")
-		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
 		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 	})
 }
