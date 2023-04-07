3f98df3d4d5b2268c4176be5a60b9f59cc667c05
Egor Fortov <egorfortov@gmail.com>
2023-04-08 00:06:08 +0300

    some bad things

--- internal/server/app.go
+++ internal/server/app.go
@@ -21,7 +21,7 @@ import (
 func StartServer() {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(gzip.GZIPHandler) // ДОБАВИЛИ сжатие трафика
+	r.Use(gzip.CheatedHandler) // ДОБАВИЛИ сжатие трафика
 
 	var cfg configs.Config
 	if err := env.Parse(&cfg); err != nil {
